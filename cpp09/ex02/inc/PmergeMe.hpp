2c3ce4fad0d3e4925aa2210b51da91581ac0fed2
jsebasti <jsebasti@student.42barcelona.com>
2024-01-28 07:11:22 +0100

    all

--- cpp09/ex02/inc/PmergeMe.hpp
+++ cpp09/ex02/inc/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:29:42 by jsebasti          #+#    #+#             */
-/*   Updated: 2024/01/16 14:24:06 by jsebasti         ###   ########.fr       */
+/*   Updated: 2024/01/25 15:29:23 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,20 @@ private:
 	
 	PmergeMe	operator=( const PmergeMe &srcChain );
 
-	static void	sortVector( std::vector<int> &vectorOrder );
+	static void				sortVector( std::vector<int> &vectorOrder );
 
-	static std::unordered_set<int, int>	createPairs( std::vector<int> &vectorOrder, bool odd );
+	static void				createA(std::vector<int> &v, std::vector<int> &A);
 
-	static void	stringNumber( std::string num );
-	static void	repeatedNumbers( std::vector<int> &vectorOrder );
-	static void parse( std::string *av, int ac, std::vector<int> &vectorOrder, std::deque<int> &dequeOrder);
+	static std::vector<int>	createPairs( std::vector<int> &vectorOrder );
+	static void				checkOrderInPairs(std::vector<int> &tmp);
+	static void				orderPairs(std::vector<int> &tmp);
+
+	static void				stringNumber( std::string num );
+	static void				repeatedNumbers( std::vector<int> &vectorOrder );
+	static void 			parse( std::string *av, int ac, std::vector<int> &vectorOrder, std::deque<int> &dequeOrder);
 
 public:
-	static void	order( std::string *av, int ac );
+	static void				order( std::string *av, int ac );
 };
 
 #endif
--- cpp09/ex02/src/PmergeMe.cpp
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 17:36:37 by jsebasti          #+#    #+#             */
-/*   Updated: 2024/01/16 14:25:14 by jsebasti         ###   ########.fr       */
+/*   Updated: 2024/01/25 15:29:19 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,28 +78,111 @@ void	PmergeMe::parse( std::string *av, int ac, std::vector<int> &vectorOrder, st
 	}
 }
 
-void	PmergeMe::createPairs( std::vector<int> &vectorOrder, bool odd ) {
-	int len = vectorOrder.size();
+void	PmergeMe::orderPairs(std::vector<int> &tmp)
+{
+	int len = tmp.size();
+	int last = 0;
 
-	if (odd == true)
-		len -= 1;
-	for (int i = 0; i < len; i++)
+	for (int j = 2; j < len; j += 2)
 	{
+		if (last != j && tmp[last] > tmp[j])
+		{
+			int auxMin = tmp[j];
+			int auxMax = tmp[j + 1];
+			tmp[j] = tmp[last];
+			tmp[j + 1] = tmp[last + 1];
+			tmp[last] = auxMin;
+			tmp[last + 1] = auxMax;
+		}
+		last += j;
+	}
+}
 
+void	PmergeMe::createA(std::vector<int> &v, std::vector<int> &A)
+{
+	int len = v.size();
+	int pairs = len / 2;
+	int i = 0;
+
+	for (int j = 1; j < pairs; j++)
+	{
+		A.push_back(v[i]);
+		i += 2;
+	}
+	len = A.size();
+	v.erase(v.begin());
+	while ((int)v.size() > len + 1)
+		v.erase(v.begin() + 1);
+}
+
+void	PmergeMe::checkOrderInPairs(std::vector<int> &tmp)
+{
+	int len = tmp.size();
+	int pair = len / 2;
+	int i = 0;
+
+	for (int j = 1; j < pair; j++)
+		i += 2;
+	if (tmp[i] > tmp[i + 1])
+	{
+		int aux = tmp[i];
+		tmp[i] = tmp[i + 1];
+		tmp[i + 1] = aux;
+	}
+}
+
+std::vector<int>	PmergeMe::createPairs( std::vector<int> &vectorOrder ) {
+	int len = vectorOrder.size();
+	int	pairs = len / 2;
+	std::vector<int> tmp;
+
+	for (int i = 0; i < pairs; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			tmp.push_back(vectorOrder[j]);
+		}
+		for (int j = 0; j < 2; j++)
+			vectorOrder.erase(vectorOrder.begin());
+		checkOrderInPairs(tmp);
+	}
+	return (tmp);
+}
+
+void	pushSInA(std::vector<int> &A, std::vector<int> &S)
+{
+	int last, next;
+	for(int i = 0; i < (int)S.size(); i++)
+	{
+		for (j = 0; j < (int)A.size(); j++)
+		{
+
+		}
 	}
 }
 
 void	PmergeMe::sortVector( std::vector<int> &vectorOrder ) {
-	bool	odd = true;
-	int		last;
+	bool	odd = vectorOrder.size() % 2 != 0;
+	int	straggler;
+	std::vector<int> A;
+	std::vector<int> S;
 
-	if (vectorOrder.size() % 2 == 0)
+	if (odd == true)
 	{
-		odd = false;
-		last = vectorOrder[vectorOrder.size()];
+		straggler = vectorOrder[vectorOrder.size() - 1];
+		vectorOrder.pop_back();
 	}
-	std::unordered_set<int, int> pairs = createPairs(vectorOrder, odd);
-
+	std::vector<int> split_vector = createPairs(vectorOrder);
+	for (int i = 0; i < (int)split_vector.size(); i++)
+	{
+		vectorOrder.push_back(split_vector[i]);
+	}
+	orderPairs(vectorOrder);
+	createA(vectorOrder, A);
+	S = vectorOrder;
+	if (odd == true)
+		S.push_back(straggler);
+	pushSInA(A, S);
 }
 
 void	PmergeMe::order( std::string *av, int ac ) {
@@ -128,8 +211,8 @@ void	PmergeMe::order( std::string *av, int ac ) {
 	double totalTimeVector = static_cast<double>(endTimeVector - startTimeVector) / CLOCKS_PER_SEC * 1000000.0;
 	double totalTimeDeque = static_cast<double>(endTimeDeque - startTimeDeque) / CLOCKS_PER_SEC * 1000000.0;
 	std::cout << "After:";
-	for (int i = 0; i < ac; i++)
-		std::cout << " " << dequeOrder[i];
+	for (int i = 0; i < (int)vectorOrder.size(); i++)
+		std::cout << " " << vectorOrder[i];
 	std::cout << std::endl;
 	std::cout << std::fixed;
 
